96fc327c0db69f5b03ec09f7961c46af8505b691
saratorres08 <saratorrez3220@gmail.com>
2024-09-18 07:18:23 -0500

    Add files via upload

---
+++ Bebida.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurante
+{
+    class Bebida : Producto
+    {
+        public Bebida(string nombre, double precio) : base(nombre, precio, "Bebida") { }
+
+        public override string MostrarDetalles()
+        {
+            return $"Bebida: {Nombre} - Precio: ${Precio}";
+        }
+    }
+}
